1d267f1bf3f9ad255f9134df3a7ec41333d6bfeb
azeem521 <azeemsre1@gmail.com>
2023-06-04 09:24:37 +0530

    till video 63

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(adminRouter);
+app.use('/admin',adminRouter);
 app.use(shopRouter);
 app.use((req,res,next)=>{
     res.status(404).send("<h1>PaGe NoT FoUnD</h1>")
